eeb2107c82daac429e683cc16de557b2aa09793e
David Geo Holmes <geometryzen@gmail.com>
2023-07-11 11:24:57 -0400

    FastButton, FastCard

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-export { FoundationElementRegistryElement, provideReactWrapper, ReactEventMap, ReactEvents, ReactWrapper, ReactWrapperConfig, ReactWrapperProps } from "@microsoft/fast-react-wrapper";
+export {
+    FoundationElementRegistryElement,
+    provideReactWrapper,
+    ReactEventMap,
+    ReactEvents,
+    ReactWrapper,
+    ReactWrapperConfig,
+    ReactWrapperProps
+} from "@microsoft/fast-react-wrapper";
+import {
+    fastButton, fastCard, provideFASTDesignSystem
+} from '@microsoft/fast-components';
+import { provideReactWrapper } from '@microsoft/fast-react-wrapper';
+import React from 'react';
 
+const { wrap } = provideReactWrapper(React, provideFASTDesignSystem());
+
+export const FastCard = wrap(fastCard());
+export const FastButton = wrap(fastButton());
